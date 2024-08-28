eea73541fd5178e8094183e1422dee15e1915bbc
Lohitha Markapuram <153831557+Lohithamarkapuram@users.noreply.github.com>
2024-08-28 20:16:54 +0530

    Add files via upload

---
+++ LetterLPattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LetterLPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the letter 'L': ");
+        int size = scanner.nextInt();
+        scanner.close();
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("*");
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print("* ");
+        }
+    }
+}
