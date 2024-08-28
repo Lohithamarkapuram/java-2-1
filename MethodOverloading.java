a12829567a5f50e92f58a76310dc5acf541c9c45
Lohitha Markapuram <153831557+Lohithamarkapuram@users.noreply.github.com>
2024-08-28 20:14:01 +0530

    Add files via upload

---
+++ MethodOverloading.java
@@ -0,0 +1,26 @@
+public class MethodOverloading{
+
+    public void display(int number) {
+        System.out.println("Single parameter method called with an integer: " + number);
+    }
+
+    public void display(String text) {
+        System.out.println("Single parameter method called with a string: " + text);
+    }
+
+    public void display(int number, String text) {
+        System.out.println("Multiple parameters method called with an integer: " + number + " and a string: " + text);
+    }
+
+    public void display(double decimal, boolean flag) {
+        System.out.println("Multiple parameters method called with a double: " + decimal + " and a boolean: " + flag);
+    }
+
+    public static void main(String[] args) {
+        MethodOverloading example = new MethodOverloading();
+        example.display(10);
+        example.display("Lohitha");
+        example.display(20, "Overloaded Method");
+        example.display(15.5,true);
+}
+}
