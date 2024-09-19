1813946bc5834892de645b88267641206eb7e33e
Lohitha Markapuram <153831557+Lohithamarkapuram@users.noreply.github.com>
2024-09-19 11:29:26 +0530

    Update Rectangle1.java

--- Rectangle1.java
+++ Rectangle1.java
@@ -1,4 +1,4 @@
-
+//8)Write a Java program to create a class called Rectangle with private instance variables length and width. Provide public getter and setter methods to access and modify these variables.
 public class Rectangle1 {
 	private int length, width;
 	int get() {
